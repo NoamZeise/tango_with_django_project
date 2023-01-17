e1d1502e82f53dd66f6bc1062d93f4fb9c9ffa08
NoamZeise <noam4134@gmail.com>
2023-01-17 16:37:44 +0000

    Finished chapter 4 - passed book tests

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
     # match template in index.html
-    context_dict = { 'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!' }
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     # render fills in templates using the dict
     return render(request, 'rango/index.html', context=context_dict)
 
+
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    # render fills in templates using the dict
+    return render(request, 'rango/about.html')
