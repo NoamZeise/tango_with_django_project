2bf901b76614bff43d61692f94c9e95da1e16684
NoamZeise <noam4134@gmail.com>
2023-01-23 23:36:26 +0000

    ch7 add category working

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def index(request):
     # show top 5 categories - the minus sign means in descending order of likes
@@ -38,3 +39,21 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST': #was data submitted
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+
+            form.save(commit=True)
+
+            return redirect('/rango/')
+
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form':form }) 
